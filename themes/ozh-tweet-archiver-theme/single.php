11c5d0d4194219b9eec490e96dee3a780e779a6b
S. Qiouyi Lu <s@qiouyi.lu>
2022-11-14 23:41:10 -0800

    Rewrite with semantic tags

--- themes/ozh-tweet-archiver-theme/single.php
+++ themes/ozh-tweet-archiver-theme/single.php
@@ -3,25 +3,27 @@
 <?php if (have_posts()) : ?>
 
 <!-- !BEGIN #tweet -->
-<div id="tweets">
+<main>
 <?php while (have_posts()) : the_post(); ?>
 
   <h2>Tweet #<?php do_action( 'ozh_ta_id' ); ?></h2>
-  <div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-    <div class="tweet">
+  <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+    <section>
       <?php the_content(); ?>
-    </div>
-    <?php get_template_part( 'post_meta' ); ?>
-  </div>
+    </section>
+    <footer>
+	    <?php get_template_part( 'post_meta' ); ?>
+  	<footer>
+  </article>
 
 <?php endwhile; ?>
 
-    <div class="pagination">
+    <nav class="pagination">
       <div class="older"><?php previous_post_link('%link', '&laquo; Previous tweet') ?></div>
       <div class="newer"><?php next_post_link('%link', 'Next tweet &raquo;') ?></div>
-    </div>
+    </nav>
 
-</div>
+</main>
 <!-- END #tweet -->
 
 
