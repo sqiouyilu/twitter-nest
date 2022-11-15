40f19e7264530efba4a5ef14675ee631c853e7d2
S. Qiouyi Lu <s@qiouyi.lu>
2022-11-15 00:32:30 -0800

    Fix icon references

--- themes/ozh-tweet-archiver-theme/footer.php
+++ themes/ozh-tweet-archiver-theme/footer.php
@@ -8,8 +8,8 @@
 
 <footer id="credits">
   <p>Powered by
-  <a href="http://wordpress.org/"><i class="fa fa-wordpress"></i> WordPress</a>
-  and <a href="https://github.com/sqiouyilu/twitter-nest"><i class="fa fa-house-signal"></i> Twitter Nest</a>.
+  <a href="http://wordpress.org/"><i class="fa-brands fa-wordpress-simple"></i> WordPress</a>
+  and <a href="https://github.com/sqiouyilu/twitter-nest"><i class="fa-solid fa-house-signal"></i> Twitter Nest</a>.
   <?php echo get_num_queries(); ?> queries in <?php timer_stop(true,2); ?> seconds.</p>
 </footer>
 <?php wp_footer(); ?>
