3d6b78371e7145016d292b24c1312eee6883e340
S. Qiouyi Lu <s@qiouyi.lu>
2022-11-14 23:39:16 -0800

    Rewrite with semantic tags

--- themes/ozh-tweet-archiver-theme/page-tweets.php
+++ themes/ozh-tweet-archiver-theme/page-tweets.php
@@ -24,26 +24,28 @@ query_posts( $args );
 <?php if (have_posts()) : ?>
 
 <!-- !BEGIN #tweets -->
-<div id="tweets">
+<main>
 
 <h2>Recent tweets <?php if( $paged > 1 ) echo "&laquo; page $paged"; ?></h2>
 
 <?php while (have_posts()) : the_post(); ?>
-  <div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-    <div class="tweet <?php echo otat_get_tweet_style(); ?>">
+  <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+    <section class="tweet <?php echo otat_get_tweet_style(); ?>">
         <?php the_content(); ?>
-    </div>
-    <?php get_template_part( 'post_meta' ); ?>
-  </div>
+    </section>
+    <footer>
+	    <?php get_template_part( 'post_meta' ); ?>
+  	</footer>
+  </article>
 
 <?php endwhile; ?>
 
-    <div class="pagination">
+    <nav class="pagination">
         <div class="older"><?php next_posts_link('&laquo; Older tweets', $wp_query->max_num_pages) ?></div>
         <div class="newer"><?php previous_posts_link('Newer tweets &raquo;') ?></div>
-    </div>
+    </nav>
 
-</div>
+</main>
 <!-- END #tweets -->
 <?php 
 // clean up after our query
