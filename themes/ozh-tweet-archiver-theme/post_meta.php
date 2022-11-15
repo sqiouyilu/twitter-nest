db3bec3a393ca74defe18b8e621322dc4569bcd9
S. Qiouyi Lu <s@qiouyi.lu>
2022-11-15 00:48:26 -0800

    Update fontawesome reference

--- themes/ozh-tweet-archiver-theme/post_meta.php
+++ themes/ozh-tweet-archiver-theme/post_meta.php
@@ -17,7 +17,7 @@
     
     <br />
     
-    <a href="<?php do_action( 'ozh_ta_tweet_link' ); ?>" title="Original on Twitter"><i class="fa fa-twitter"></i> Original</a>
+    <a href="<?php do_action( 'ozh_ta_tweet_link' ); ?>" title="Original on Twitter"><i class="fa-brands fa-twitter"></i> Original</a>
     <a href="<?php the_permalink() ?>" title="Permalink" rel="bookmark"><i class="fa fa-bookmark"></i> Local Permalink</a>
     &mdash; <a href="https://twitter.com/intent/tweet?in_reply_to=<?php do_action( 'ozh_ta_id' ); ?>"><i class="fa fa-reply"></i> Reply</a>
     <a href="https://twitter.com/intent/retweet?tweet_id=<?php do_action( 'ozh_ta_id' ); ?>"><i class="fa fa-retweet"></i> Retweet</a>
