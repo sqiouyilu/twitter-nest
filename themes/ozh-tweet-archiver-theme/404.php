af10411644f269258a4076f8a1de2682904b3dc9
S. Qiouyi Lu <s@qiouyi.lu>
2022-11-20 21:39:14 -0800

    Delete old theme (ozh-tweet-archiver-theme)
    Replace with rewritten theme.

--- themes/ozh-tweet-archiver-theme/404.php
+++
@@ -1,5 +0,0 @@
-<?php get_header(); ?>
-
-<h2>Sorry, nothing found here (404)</h2>
-
-<?php get_footer(); ?>
--- themes/ozh-tweet-archiver-theme/searchform.php
+++
@@ -1,3 +0,0 @@
-<form method="get" id="searchform" action="/tweets/">
-  <input type="text" name="s" id="s" value="<?php the_search_query(); ?>" class="txt" /><input type="submit" id="searchsubmit" value="&nbsp;" class="btn" />
-</form>
